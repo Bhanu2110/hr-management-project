524a0bccadc1c52862b97f8fd69fb33dacd06ed3
Bhanu2110 <bhanu.gunda@syncallsolutions.com>
2025-12-05 11:00:26 +0530

    Fix import path for SalarySlipPdfGenerator

--- src/components/salary/SalaryManagement.tsx
+++ src/components/salary/SalaryManagement.tsx
@@ -56,7 +56,7 @@ import { SalarySlip, SalaryStructure, SalaryCreateRequest, MONTHS, SALARY_STATUS
 import { supabase } from "@/integrations/supabase/client";
 import { toast } from "sonner";
 import { SalarySlipView } from "./SalarySlipView";
-import { downloadSalarySlipPDF } from "@/utils/salarySlipPdfGenerator";
+import { downloadSalarySlipPDF } from "@/utils/SalarySlipPdfGenerator";
 
 interface SalaryManagementProps {
   employees?: Array<{
